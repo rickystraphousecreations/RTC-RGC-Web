0f91ddf16f65abdf6439901053f1c16e2cbceb99
rickystraphousecreations <rickystraphousecreations@gmail.com>
2026-07-31 15:38:01 -0400

    updated files

--- loader.js
+++ loader.js
@@ -3,12 +3,27 @@ const params = new URLSearchParams(window.location.search);
 const game = params.get("g");
 
 if (!game) {
+    document.body.innerHTML = `
+        <h1 style="
+            color:white;
+            font-family:Arial;
+            text-align:center;
+            margin-top:20%;
+        ">
+            No Game Selected
+        </h1>
+    `;
+} else {
+    document.body.innerHTML = `
+        <h1 style="
+            color:white;
+            font-family:Arial;
+            text-align:center;
+            margin-top:20%;
+        ">
+            Loading ${game}...
+        </h1>
+    `;
 
-    document.body.innerHTML =
-        "<h1 style='color:white;text-align:center;margin-top:20%'>No Game Selected</h1>";
-
-    throw new Error("No game selected.");
-
+    console.log("Loading:", game);
 }
-
-console.log("Loading:", game);
